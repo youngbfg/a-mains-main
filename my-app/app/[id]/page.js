5c27df2c82d7123a880a69459dabed435d817555
youngbfg <bilalfurkan@gmail.com>
2023-09-13 09:51:25 +0300

    v0.3

---
+++ my-app/app/[id]/page.js
@@ -0,0 +1,88 @@
+"use client";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from "next-share";
+import styles from "./styles.module.css";
+import {
+  AiFillFacebook,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
+import "moment/locale/tr";
+
+async function getPost(id) {
+  try {
+    const response = await fetch(`https://dummyjson.com/posts/${id}`);
+    return response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+// test
+export default async function Page({ params }) {
+  const { id, title, body, userId } = await getPost(params.id);
+  if (!title) {
+    return notFound();
+  }
+
+  return (
+    
+    <div className={styles.blogContainer}>
+      <div className={styles.cardImage}>
+        <Image
+          src={`https://picsum.photos/960/400?random=${id}`}
+          alt="/"
+          fill
+        />
+      </div>
+      <div className={styles.smallImage}>
+        <img
+          alt="/"
+          className={styles.smallImage}
+          src={`https://picsum.photos/960/400?random=${id}`}
+        />
+        <div className={styles.text}>userId:{userId}</div>
+        <div className={styles.texts}>8 Mayıs 2023 / İstanbul </div>
+      </div>
+      
+      <div className={styles.title}>{title}</div>
+      <p className={styles.titles}>{body}</p>
+      <div className={styles.iconas}>
+        <FacebookShareButton url={`http://10.1.245.102:3000/${id}`}>
+          <AiFillFacebook />
+        </FacebookShareButton>
+        <TwitterShareButton url={`http://192.168.68.111:3000/${id}`}>
+          <AiFillTwitterCircle  />
+        </TwitterShareButton>
+        <LinkedinShareButton url={`http://192.168.68.111:3000/${id}`}>
+          <AiFillLinkedin />
+        </LinkedinShareButton>
+        <WhatsappShareButton url={`http://192.168.68.111:3000/${id}`}>
+          <AiOutlineWhatsApp />
+        </WhatsappShareButton>
+        </div>
+        <div className={styles.icona}>
+        <FacebookShareButton url={`http://10.1.245.102:3000/${id}`}>
+          <AiFillFacebook />
+        </FacebookShareButton>
+        <TwitterShareButton url={`http://192.168.68.111:3000/${id}`}>
+          <AiFillTwitterCircle  />
+        </TwitterShareButton>
+        <LinkedinShareButton url={`http://192.168.68.111:3000/${id}`}>
+          <AiFillLinkedin />
+        </LinkedinShareButton>
+        <WhatsappShareButton url={`http://192.168.68.111:3000/${id}`}>
+          <AiOutlineWhatsApp />
+        </WhatsappShareButton>
+        </div>
+        </div>
+    
+    
+  );
+}
---
+++ my-app/components/FlashNews/index.js
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./styles.module.css";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from "next-share";
+
+import {
+  AiFillFacebook,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
+import moment from "moment";
+import "moment/locale/tr";
+
+export default async function FlashNews({ title, body, id }) {
+  return (
+    <div>
+      <Link href={`/${id}`} className={styles.cards}>
+        <div className={styles.cardImage}>
+          <Image
+            src={`https://picsum.photos/200/300?random=${id}`}
+            alt="Blog Card Image"
+            fill
+           
+          />
+        </div>
+        <div className={styles.cardBody}>
+          <h3 className={styles.title}>{title}</h3>
+          <div className={styles.time}>{moment(id).format("LLL")}</div>
+          <p className={styles.body}>{body}</p>
+          <button className={styles.buttons}>Daha fazla Oku</button>
+          <div className={styles.icons}>
+            <FacebookShareButton url={`http://10.1.245.102:3000/${id}`}>
+              <AiFillFacebook />
+            </FacebookShareButton>
+            <LinkedinShareButton url={`http://192.168.68.111:3000/${id}`}>
+              <AiFillLinkedin />
+            </LinkedinShareButton>
+            <TwitterShareButton url={`http://192.168.68.111:3000/${id}`}>
+              <AiFillTwitterCircle />
+            </TwitterShareButton>
+            <WhatsappShareButton url={`http://192.168.68.111:3000/${id}`}>
+              <AiOutlineWhatsApp  />
+            </WhatsappShareButton>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
