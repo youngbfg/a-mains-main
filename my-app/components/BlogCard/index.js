5d69983cd040fe711d5a73e8350cd97f16236d8f
youngbfg <bilalfurkan@gmail.com>
2023-09-13 09:55:27 +0300

    aaa

--- my-app/components/BlogCard/index.js
+++ my-app/components/BlogCard/index.js
@@ -19,6 +19,7 @@ import "moment/locale/tr";
 import styles from "./styles.module.css";
 
 
+
 export default async function BlogCard({ title, body, id }) {
   return (
     <Link href={`/${id}`} className={styles.card}>
